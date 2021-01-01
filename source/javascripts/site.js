b70827bc86765795a7cf81c99f9942c2d0c1c22a
TheE <thee@mywarp.org>
2021-01-01 20:52:14 +0100

    Fix navigation on mobile

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,5 +3,6 @@ var menuNav = document.getElementById('main-navigation');
 menuButton.addEventListener('click', function(e) {
     menuButton.classList.toggle('is-active');
     menuNav.classList.toggle('is-active');
+    document.body.classList.toggle('noscroll');
     e.preventDefault();
 });
