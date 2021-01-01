88e928deb8d86dd20ff6bf7ceb7ef6fd2e1e11f5
TheE <TheE@users.noreply.github.com>
2021-01-01 19:54:03 +0000

    Deploying to master from  @ b70827bc86765795a7cf81c99f9942c2d0c1c22a 🚀

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,1 @@
-var menuButton=document.getElementById("menu-button"),menuNav=document.getElementById("main-navigation");menuButton.addEventListener("click",function(t){menuButton.classList.toggle("is-active"),menuNav.classList.toggle("is-active"),t.preventDefault()});
+var menuButton=document.getElementById("menu-button"),menuNav=document.getElementById("main-navigation");menuButton.addEventListener("click",function(t){menuButton.classList.toggle("is-active"),menuNav.classList.toggle("is-active"),document.body.classList.toggle("noscroll"),t.preventDefault()});
