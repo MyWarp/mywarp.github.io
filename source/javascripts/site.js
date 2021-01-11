b2ccd1eafbfc39d676abe7554c795b7b8aebe4f3
TheE <thee@mywarp.org>
2021-01-11 13:20:54 +0100

    Add proper darkmode for the particles

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,8 +1,34 @@
+var bodyStyle = getComputedStyle(document.body);
 var menuButton = document.getElementById('menu-button');
 var menuNav = document.getElementById('main-navigation');
+
 menuButton.addEventListener('click', function(e) {
     menuButton.classList.toggle('is-active');
     menuNav.classList.toggle('is-active');
     document.body.classList.toggle('noscroll');
     e.preventDefault();
 });
+
+function setup_particles() {
+    var color_var = bodyStyle.getPropertyValue('--particles-color').trim();
+
+    tsParticles.loadJSON('site-canvas', 'default.json')
+        .then((container) => {
+            container.options.load({
+                particles: {
+                    color: {
+                        value: color_var
+                    },
+                    line_linked: {
+                        color: {
+                            value: color_var
+                        }
+                    }
+                }
+            });
+            container.refresh();
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
