f2e4eb9b1b4e71b4abb91484b40094e5b942ff20
TheE <TheE@users.noreply.github.com>
2021-01-31 17:56:44 +0000

    Deploying to master from  @ 99038388694929db995261157aad2c79ceccc4ba 🚀

---
+++ assets/javascripts/site.js
@@ -0,0 +1,2 @@
+!function(){var t=document.getElementById("menu-button"),e=document.getElementById("main-navigation");t.addEventListener("click",(function(n){t.classList.toggle("is-active"),e.classList.toggle("is-active"),document.body.classList.toggle("noscroll"),n.preventDefault()}))}();
+//# sourceMappingURL=site.js.map
