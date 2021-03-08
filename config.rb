1ab94bf744ed31e1a8af210b1ad0a0c3f572b016
TheE <thee@mywarp.org>
2021-03-08 12:48:58 +0100

    Update favicons

--- config.rb
+++ config.rb
@@ -66,11 +66,13 @@ configure :build do
   activate :favicon_maker do |f|
     f.template_dir  = 'source/images'
     f.icons = {
-      "_favicon.svg" => [
-        { icon: "apple-touch-icon.png", size: "180x180" },
+      "_favicon_small.svg" => [
         { icon: "favicon-32x32.png" },
         { icon: "favicon-16x16.png" },
         { icon: "favicon.ico", size: "48x48,32x32,16x16" },
+      ],
+      "_favicon_large.svg" => [
+        { icon: "apple-touch-icon.png", size: "180x180" },
         { icon: "mstile-70x70.png", size: "70x70" },
         { icon: "mstile-144x144.png", size: "144x144" },
         { icon: "mstile-150x150.png", size: "150x150" },
