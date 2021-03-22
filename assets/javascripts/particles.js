1e09697ad9d568f0a59dba7eb895c73b9d060313
TheE <thee@mywarp.org>
2021-03-22 16:40:06 +0100

    Move tsParticles config into particles.js

--- assets/javascripts/particles.js
+++ assets/javascripts/particles.js
@@ -1,24 +1,65 @@
 import { tsParticles } from "tsparticles";
-
 var bodyStyle = getComputedStyle(document.body);
 var color_var = bodyStyle.getPropertyValue('--particles-color').trim();
-
-tsParticles.loadJSON('site-canvas', 'default.json')
-    .then((container) => {
-        container.options.load({
-            particles: {
-                color: {
-                    value: color_var
-                },
-                line_linked: {
-                    color: {
-                        value: color_var
-                    }
-                }
-            }
-        });
-        container.refresh();
-    })
-    .catch((error) => {
-        console.error(error)
-    })
+tsParticles.load('site-canvas', {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 2500
+      }
+    },
+    "color": {
+      "value": color_var
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": color_var
+      },
+      "polygon": {
+        "nb_sides": 5
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 180,
+      "color": color_var,
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "retina_detect": true
+}).catch((error) => {
+  console.error(error)
+});
